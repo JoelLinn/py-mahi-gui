b1ca454ab832ba8e23fd395a2e4c42cee673c8a0
Joel Linn <jl@conductive.de>
2020-10-21 02:31:37 +0200

    Bump to v0.2.3

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mahi_gui',
-    version='0.2.2',
+    version='0.2.3',
     author='Joel Linn',
     author_email='jl@conductive.de',
     description='Python bindings for mahi-gui imgui and implot.',
